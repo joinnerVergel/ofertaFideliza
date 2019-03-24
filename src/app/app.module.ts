42afbbaa13abdc94c1c25afed3a2ef037b74752a
JOINNER KALET VERGEL <JKVERGELOT@nh.inet>
2019-03-24 12:15:18 -0500

    componente home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-
+import {MatToolbarModule} from '@angular/material';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent
   ],
   imports: [
+    MatToolbarModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+   
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
